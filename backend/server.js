ca202219fcf0b7c05f62bcbdff32fa54decb73a3
Antonio Furtado <antoniordf@hotmail.com>
2024-03-16 01:54:21 +0000

    minor update

--- backend/server.js
+++ backend/server.js
@@ -28,6 +28,8 @@ const cache = {};
 app.post("/generate-pdf", async (req, res) => {
   const cacheKey = sortedStringify(req.body);
 
+  setTimeout(() => {}, 2000);
+
   if (cache[cacheKey]) {
     if (Date.now() - cache[cacheKey].timestamp < CACHE_DURATION) {
       res.json(cache[cacheKey]);
