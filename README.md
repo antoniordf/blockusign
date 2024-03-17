79c13da3163974924cf690099cfdb2b8359d1c26
miqlar <miquellarsson@gmail.com>
2024-03-17 05:57:58 +0000

    updated contract, readme

--- README.md
+++ README.md
@@ -4,11 +4,13 @@
   <img src="img/Blocusign_logo.png" alt="Logo" width="300">
 </p>
 
-## Summary
+## Description
 
 ```
+This project combines Multi-sigs with Account Abstraction & Off-chain compute to allow decentralized organizations to sign PDF documents without .  This would allow DAOs to produce invoice-documents, tax-forms, and other legal documents in a more decentralized fashion. This is crucial for on-boarding the next-generation of Web3 users since it allows Web3 organizations/entities to trustlessly satisfy obligations in the Web2/legal world.
+How its made: 
 
-
+We used Safe AA SDK to produce a multi-sig Smart Wallet that can send transactions without having to pay for gas. We then use Chainlink Functions to call an API that can take the data signed by the multisig and put it into a fillable PDF. We made our own API that allows Chainlink Functions to be used for POST-requests that are non-idempotent.
 ```
 
 
@@ -16,7 +18,7 @@
 
 | Network | Address | Etherscan Verified |
 | --------------- | --------------- | --- |
-| Sepolia  | [0x31554a01faEdDFDe645D6BDd8f810CBF1D180fA8](https://sepolia.etherscan.io/address/0x31554a01faEdDFDe645D6BDd8f810CBF1D180fA8)  | :white_check_mark:  |
+| Sepolia  | [0xc75af90312a4c66c294FDD32CBb56C705A33D5D7](https://sepolia.etherscan.io/address/0xc75af90312a4c66c294FDD32CBb56C705A33D5D7)  | :white_check_mark:  |
 
 
 
--- contracts/chainlinkFunction.sol
+++ contracts/chainlinkFunction.sol
@@ -15,28 +15,29 @@ interface ChanlinkFunctionsConsumer {
     ) external returns (bytes32 requestId);
 }
 
-contract docusign{
+contract blockusign{
 
     address public lastMsgSender;
     address public lastTxOrigin;
-    ChanlinkFunctionsConsumer chainlink = ChanlinkFunctionsConsumer(0x4377F894e52Dfd0921F0f8b50D45f7c7232Ec2ce); //Mumbai
+    ChanlinkFunctionsConsumer chainlink = ChanlinkFunctionsConsumer(0xA1cB7CE218a96811a8f96CC9384aBf8aaB112E74); //Sepolia
 
-    function increase() public {
+    function callChainLink() public {
 
         lastMsgSender = msg.sender;
         lastTxOrigin = tx.origin;
 
         chainlink.sendRequest(
-            "const apiResponse = await Functions.makeHttpRequest({url: `https://blockusign-05843e86beea.herokuapp.com/generate-pdf`, method: 'POST'});return Functions.encodeString(apiResponse.data.data.document);",
+            "const apiResponse = await Functions.makeHttpRequest({url: 'https://6043-213-152-241-52.ngrok-free.app/generate-pdf', method: 'POST', headers: {'Content-Type': 'application/json'}, data: {title: 'Contract 4',signature1: '0x54Ab3',signature2: '0x73c9A',etherscan: 'https://etherscan.io/tx/0xdbf40c4548ae32e4f48e8f33eecf4ed0f1447dae67425fd7b9d56ca8176ec620',txHash: '0xdbf40c4548ae32e4f48e8f33eecf4ed0f1447dae67425fd7b9d56ca8176ec620'}});return Functions.encodeString(apiResponse.data.data.document);",
             bytes(""),
             0,
             0,
             new string[](0),
             new bytes[](0),
-            1402,
+            2144,
             300000,
             0x66756e2d657468657265756d2d7365706f6c69612d3100000000000000000000
         );
     }
      
 }
+
